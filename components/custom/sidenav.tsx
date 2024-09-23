28e870f308bda300108bb6344bf408fe7640ad57
Trey Murray <treymurray00@outlook.com>
2024-09-23 13:35:42 -0400

    Create sidenav.tsx

---
+++ components/custom/sidenav.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Accordion } from '../ui/accordion'
+import { Button } from '../ui/button'
+
+const sidenav = () => {
+  return (
+    <div>sidenav</div>
+  )
+}
+
+export default sidenav
