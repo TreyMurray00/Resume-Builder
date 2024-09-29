8cd1d84bb2233271febadb596f10816ae86a771d
Trey Murray <treymurray00@outlook.com>
2024-09-29 09:15:05 -0400

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     
       <div className="flex flex-col lg:flex-row gap-8">
-        <div className="w-1/3  h-screen overflow-y-auto">
+        <div className="w-1/3  h-screen overflow-y-auto p-4 border border-r-black">
           <ResumeForm />
         </div>
         
